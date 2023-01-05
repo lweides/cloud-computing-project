046d76794c814d51e5b0b2a8916ec2e72403d481
Lukas Weidinger <lukas.weides@gmail.com>
2023-01-05 14:32:34 +0100

    Add shared datatypes

---
+++ shared/src/main/java/request/Request.java
@@ -0,0 +1,10 @@
+package request;
+
+/**
+ * Request from the outside (i.e., the client) into the k8s cluster.
+ */
+public class Request {
+    // TODO add fields for microservice B and C
+    // TODO define some logic to validate requests
+    public String foo;
+}
