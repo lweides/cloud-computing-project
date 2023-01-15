97afa5e6eadaa848dbd0719eb6e021668f46bfda
Fboeck <weissenboeck98.fabian@gmail.com>
2023-01-15 21:37:05 +0100

    Update HealthCheckResource.java

--- microservice-b/src/main/java/microservice/b/HealthCheckResource.java
+++ microservice-b/src/main/java/microservice/b/HealthCheckResource.java
@@ -4,6 +4,7 @@ package microservice.b;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
+import org.springframework.http.ResponseEntity;
 import request.Request;
 import response.Response;
 
@@ -14,29 +15,16 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("/api")
-public class Resource {
-    private static final Logger LOG = Logger.getLogger(Resource.class);
-    private static final ObjectMapper MAPPER = new ObjectMapper();
-
-    @Inject
-    @RestClient
-    Forwarder forwarder;
+public class HealthCheckResource {
+    private static final Logger LOG = Logger.getLogger(HealthCheckResource.class);
 
     @GET
     @Path("/healthcheck")
-    public Response healthcheck(Request request) {
+    public ResponseEntity<String> healthcheck(Request request) {
         LOG.info(
                 "Received healthcheck request"
         );
-        return new Response("healthcheck: microservice-b available");
-    }
-
-    @POST
-    @Path("/save")
-    public Response saveMessage(Request request) {
-        LOG.info("received message for storage");
-//        Response response = forwarder.forwardToA(request);
-        return new Response();
+        return ResponseEntity.ok("Microservice B available");
     }
 }
 
